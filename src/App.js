7210be0a6800365c1d4bd21620e1f54f8fc8ba5f
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-11-14 23:34:35 -0800

    installed axios and a get call is being made and hitting the get /questions endpoint successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
+import axios from "axios";
 
 function App() {
   useEffect(() => {
     themeChange(false);
     // 👆 false parameter is required for react project
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/questions`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
   }, []);
 
   return <div></div>;
