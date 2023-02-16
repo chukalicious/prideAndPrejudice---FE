fb1d6e1118ec61e88722aee9913564d97e095043
Katiuska AIicea <katiuskaalicea@gmail.com>
2023-02-16 03:25:37 -0800

    Revert "Revert "fonts have been installed""

--- src/components/UserDashboardComponent/Dashboard.js
+++ src/components/UserDashboardComponent/Dashboard.js
@@ -28,7 +28,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="bg-base-100">
+    <div className="bg-base-100 min-h-[65vh]">
       <div className="card mx-auto py-8 w-[75%] bg-base-100 shadow-xl image-full">
         <figure>
           <img
@@ -37,13 +37,15 @@ const Dashboard = () => {
           />
         </figure>
         <div className="card-body">
-          <h2 className="text-2xl font-bold text-center">Start a new game!</h2>
-          <p className="text-center">
+          <h2 className="text-2xl text-center font-serif capitalize">
+            Start a new game!
+          </h2>
+          <p className="text-center font-sans font-semibold text-lg">
             Test your wit and wisdom with a Pride and Prejudice trivia game. Are
             you ready?
           </p>
           <div className="card-actions justify-center mt-4">
-            <button onClick={handleClick} className="btn btn-primary">
+            <button onClick={handleClick} className="btn btn-primary font-sans">
               Start Now
             </button>
           </div>
@@ -74,12 +76,14 @@ const Dashboard = () => {
                 className="card mx-auto pb-8 w-[75%] bg-base-100 shadow-xl image-full"
               >
                 <div className="card-body">
-                  <h2 className="text-xl text-center">{question.question}</h2>
+                  <h2 className="text-xl text-justify font-serif">
+                    {question.question}
+                  </h2>
                   <div className="card-actions justify-center mt-4">
-                    <label className="label-text-alt w-full">
+                    <label className="label-text w-full font-sans text-neutral-content">
                       Pray, which answer will you choose?
                     </label>
-                    <ul className="menu menu-compact lg:menu-normal bg-base-100 w-56 p-2 rounded-box">
+                    <ul className="menu menu-compact lg:menu-normal bg-base-100 w-56 p-2 rounded-box font-serif italic font-bold text-neutral-content">
                       <li>
                         <label className="label-text">
                           <input
